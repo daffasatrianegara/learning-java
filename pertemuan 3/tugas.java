f0c819b2f4dccca3bf5bf6c2e6d568a747444ab5
daffasatrianegara <daffasatria036@gmail.com>
2021-09-14 21:51:23 +0700

    tugas pertemuan 3

---
+++ pertemuan 3/tugas.java
@@ -0,0 +1,19 @@
+public class tugas {
+    public static void main(String[] args){
+        String nama = "muhammad daffa satria negara";
+        String NIM = "21537144027";
+        int angkatan = 3;
+        String prodi = "teknologi informasi";
+        String jurusan = "jurusan pendidikan teknik elektronika dan informatika";
+        String fakultas = "teknik";
+        double ipkHarapan = 3.8 ;
+        System.out.println("perkenalkan nama saya " + nama);
+        System.out.println("nomor NIM saya adalah " + NIM);
+        System.out.println("saya dari fakultas " + fakultas);
+        System.out.println(jurusan);
+        System.out.println("prodi " + prodi);
+        System.out.println("di prodi teknologi informasi ini saya berada di angkatan ke " + angkatan);
+        System.out.println("harapan saya di semester satu ini saya mendapatkan ipk sebesar " + ipkHarapan);
+
+    }
+}
---
+++ pertemuan 3/tugas.py
@@ -0,0 +1,15 @@
+nama = "muhammad daffa satria negara"
+NIM = 21537144027
+angkatan = 3
+prodi = "teknologi informasi"
+jurusan = "jurusan pendidikan teknik elektronika dan informatika"
+fakultas = "teknik"
+ipkHarapan = 3.5
+
+print("perjenalkan nama saya " + nama)
+print("NIM saya adalah " + str(NIM))
+print("saya dari fakultas " + fakultas)
+print(jurusan)
+print("dan dari prodi " + prodi)
+print("di prodi ini saya berada di angkatan ke " + str(angkatan))
+print("harapan saya di semester ini saya mendapatkan IPK sebesar " + str(3.5))

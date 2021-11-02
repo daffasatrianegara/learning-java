f4fcfa625385bc63c0e80662512412418fe6bb1e
daffasatrianegara <daffasatria036@gmail.com>
2021-11-02 12:01:01 +0700

    baru

--- pertemuan 8/tugas8.java
+++ pertemuan 8/tugas8.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 
-public class tugas8 {
+class Tugas8 {
     public static void main(String[] args) {
         String[] nama = new String[1000];
         double[] ipk = new double[1000];
-        for (int i = 1; i <= 1000; i++) {
+        String[] exit = {"exit", "Exit"};
+        int i;
+        double total = 0;
+        for (i = 1; i <= 1000; i++) {
             Scanner scan = new Scanner(System.in);
-            System.out.println("memasukkan data");
-            System.out.println("Data " + i);
-            System.out.print("masukkan nama anda : ");
-            nama[i] = scan.nextLine();
-            if (name == i) {
+            System.out.println("Data " + i + " : ");
+            System.out.print("Nama Mahasiswa = ");
+            String namadata = scan.nextLine();
+            if (namadata.equals(exit[0]) || namadata.equals(exit[1])) {
                 break;
             } else {
-                nama[i] = nama;
+                nama[i] = namadata;
             }
-            System.out.print("masukkan IPK anda : ");
-            ipk[i] = scan.nextDouble();
+            System.out.print("IPK Mahasiswa = ");
+            double ipkdata = scan.nextDouble();
+            ipk[i] = ipkdata;
+            System.out.println();
+            total = total + ipk[i];
         }
+            System.out.println("NO   Nama      IPK      ");
+            System.out.println("------------------------");
+        for (int j = 1; j < i; j++) {
+            System.out.println(j + "   " + nama[j] + "      " + ipk[j] + "      ");
+        }
+        System.out.println("------------------------");
+        System.out.println("Rata - rata = " + total / (i - 1));
     }
 }
---
+++ pertemuan 9/kalkulator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class kalkulator {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukan nilai A = ");
+        int nilaiA = scan.nextInt();
+        System.out.print("Masukan nilai B = ");
+        int nilaiB = scan.nextInt();
+
+        int hasil = penjumlahan(nilaiA, nilaiB);
+        System.out.println("Nilai A + B = " + hasil);
+    }
+
+    public static int penjumlahan(int a, int b) {
+        int hasil = a + b;
+        return hasil;
+    }
+}

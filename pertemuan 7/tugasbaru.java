18c6a7f3e6bb468b8754271ef34d1ecdea0cb93e
daffasatrianegara <daffasatria036@gmail.com>
2021-11-24 19:43:16 +0700

    baru

--- pertemuan 7/tugasbaru.java
+++ pertemuan 7/tugasbaru.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class tugasbaru {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner ya = new Scanner(System.in);
         System.out.print("masukkan nilai n : ");
-        int n = Scan.nextInt();
+        int n = ya.nextInt();
 
         if (n % 2 == 0) {
             for (int i = 1; i <= n + 1; i++) {
